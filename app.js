580e14d86251d50e2fdacec966c75b8ad7952755
Josh Chang <mhci@Joshs-Macintosh.local>
2015-12-21 20:29:09 +0800

    refactor(code): update express and reformat routing

--- app.js
+++ app.js
@@ -1,40 +1,60 @@
-/**
- * Link to Parse
- */
-GLOBAL.Parse = require('parse/node').Parse;
-Parse.initialize("7Bfs3DjfQ7iPYJhg3vy0hKPf1n7xHwp7Ibq8qzOI", "jKVyO1bQFcj62shLneClSTWrEQt9IRGly5HL0Scc");
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-/**
- * Module dependencies.
- */
-var express = require('express')
-  , routes = require('./routes');
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
-var app = module.exports = express.createServer();
+var app = express();
 
-// Configuration
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
 
-// Routes
-
-app.get('/', routes.index);
-
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
-});
+module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,29 @@
+var express = require('express');
+var router = express.Router();
 
-/*
- * GET home page.
- */
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'CSIE Basketball' });
+});
 
-exports.index = function(req, res){
-  res.render('index', { title: 'CSIE Basketball' })
-};
+router.post('/', function(req,res){
+    console.log(req);
+    var user = new Parse.User();
+    
+    user.set("username", req.body.registerID);
+    user.set("password", req.body.password);
+    
+    var email = req.body.registerID + "@csie.ntu.edu.tw";
+    user.set("email", email);
+
+    user.signUp(null, {
+        success: function(user) {
+            res.render('index', { title: 'CSIE Basketball' })
+        },
+        error: function(user, error) {
+            res.render('index', { title: 'CSIE Basketball' })
+        }
+    });
+});
+
+module.exports = router;
