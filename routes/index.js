39511e10339a3655cc9539e315d1e5e7aac48d1a
michael0432 <19610217>
2016-04-19 17:47:16 +0800

    feat(login): add some error detection all error with print at console.log() because alert() has bug. login account only have to enter b0x902xxx without @ntu.edu.tw when account is empty print "please enter your account" when account is invalid print "Invalid account!" when password is empty print "please enter your password" when password is invalid print "Wrong password!"

--- routes/index.js
+++ routes/index.js
@@ -5,50 +5,69 @@ var ref = new Firebase('https://blistering-heat-5573.firebaseio.com/');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CSIE Basketball' });
+	res.render('index', { title: 'CSIE Basketball' });
 });
 
 router.post('/', function(req,res){
 
 	if (req.body.hasOwnProperty('registerSub')){
-		console.log('reg');
-		var a = req.body.registerID;
-		var b = req.body.password;
 		ref.createUser({
-			email: a,
-			password: b
+			email: req.body.registerID,
+			password: req.body.password
 		}, function(error, userData) {
 			if (error) {
 				switch (error.code) {
 					case "EMAIL_TAKEN":
-						console.log("The new user account cannot be created because the email is already in use.");
-						break;
+					console.log("The new user account cannot be created because the email is already in use.");
+					break;
 					case "INVALID_EMAIL":
-						console.log("The specified email is not a valid email.");
-						break;
+					console.log("The specified email is not a valid email.");
+					break;
 					default:
-						console.log("Error creating user:", error);
+					console.log("Error creating user:", error);
 				}
 			} else {
 				console.log("Successfully created user account with uid:", userData.uid);
 				res.redirect('/');
 			}
 		});
+		
+		
 	} else if (req.body.hasOwnProperty('loginSub')) {
 		console.log('login');
-		ref.authWithPassword({
-			email    : req.body.loginID,
-			password : req.body.password
-		}, function(error, authData) {
-			if (error) {
-				console.log("Login Failed!", error);
-			} else {
-				console.log("Authenticated successfully with payload:", authData);
-				res.redirect('/home');
-			}
-		});
+		var a = req.body.loginID;
+		var b = req.body.password;
+		console.log('test');
+		if(!a){
+			console.log("please enter your account");
+			res.redirect('/');
+		} else if(!b){
+			console.log("please enter your password");
+			res.redirect('/');
+		} else{
+			ref.authWithPassword({
+				email    : a+"@ntu.edu.tw",
+				password : b
+			}, function(error, authData) {
+				if (error) {
+					console.log("Login Failed!", error);
+					switch(error.code) {
+						case "INVALID_PASSWORD":
+						console.log("Wrong password!");
+						break;
+						case "INVALID_USER":
+						console.log("Invalid account!");
+						break;
+						default:
+						console.log("Other error!");
+					}
+				} else {
+					console.log("Authenticated successfully with payload:", authData);
+					res.redirect('/home');
+				}
+			});
+		}	
 	}
-
 });
 
 module.exports = router;
